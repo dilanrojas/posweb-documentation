e9dee38016362c6be3c518cf8bca441446b2961e
Dilan Rojas <rojasvargasdilan010@gmail.com>
2024-11-28 08:44:08 -0600

    Deploy fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import awsAmplify from 'astro-aws-amplify';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: awsAmplify()
 });
